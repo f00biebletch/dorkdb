6030417e122645cac94566348f6ab54001c0d1d4
Kevin McIntire <kevinmcintire@Kevins-MacBook-Pro.local>
2016-09-22 22:50:41 -0700

    set and get work

--- native/dorkdb.c
+++ native/dorkdb.c
@@ -31,10 +31,14 @@ typedef struct dorkdb_t {
 // public FIXIT move to .h
 dorkdb_t *init_db();
 void halt_db(dorkdb_t *db);
-void db_set(cmd_t cmd, dorkdb_t *db);
+void db_set(cmd_t *cmd, dorkdb_t *db);
+int db_get(cmd_t *cmd, dorkdb_t *db);
+void db_unset(cmd_t *cmd, dorkdb_t *db);
 void repl(dorkdb_t *db);
+cmd_t *eval(char *input);
 
 int hash(char *s);
+int exec_cmd(cmd_t *cmd, dorkdb_t *db);
 
 int main(int argc, char **argv) {
   dorkdb_t *db = init_db();
@@ -50,13 +54,61 @@ dorkdb_t *init_db() {
 }
 
 void repl(dorkdb_t *db) {
+  while (1) {
+    char input[256];
+    printf("dorkdb> ");
+    gets(input);
+    cmd_t *cmd = eval(input);
+    int result = exec_cmd(cmd, db);
+    if (result != 0) {
+      printf("%s failed: %d\n", cmd->cmd, result);
+    }
+    else {
+      printf("Success!\n");
+    }
+    free(cmd);
+  }
 }
 
-void db_set(cmd_t cmd, dorkdb_t *db) {
-  int idx = hash(cmd.var);
+cmd_t *eval(char *input) {
+  char sep[4] = " ";
+  char *c = strtok(input, sep);
+  char *k = strtok(NULL, sep);
+  char *v = NULL;
+  if (k) v = strtok(NULL, sep);
+  cmd_t *cmd = (cmd_t *)malloc(sizeof(cmd_t));
+  strcpy(cmd->cmd, c);
+  if (k) strcpy(cmd->var, k);
+  if (v) cmd->value = atoi(v);
+
+  return cmd;
+}
+
+int exec_cmd(cmd_t *cmd, dorkdb_t *db) {
+  printf("cmd: %s %s %d\n",cmd->cmd, cmd->var, cmd->value);
+  int x;
+  switch (cmd->cmd[0]) {
+  case 's':
+    db_set(cmd, db);
+    break;
+  case 'u':
+    db_unset(cmd, db);
+    break;
+  case 'g':
+    x = db_get(cmd, db);
+    printf("Get %s -> %d\n", cmd->var, x);
+    break;
+  default:
+    return -666;
+  }
+  return 0;
+}
+
+void db_set(cmd_t *cmd, dorkdb_t *db) {
+  int idx = hash(cmd->var);
   bucket_t *bucket = (bucket_t *)malloc(sizeof(bucket_t));
-  strcpy(bucket->name, cmd.var);
-  bucket->value = cmd.value;
+  strcpy(bucket->name, cmd->var);
+  bucket->value = cmd->value;
   if (db->buckets[idx] == NULL) {
     bucket->next = NULL;
     db->buckets[idx] = bucket;
@@ -68,21 +120,31 @@ void db_set(cmd_t cmd, dorkdb_t *db) {
     }
     cur->next = bucket;
   }
-  if (cmd.value < INDEX_COUNT) {
-    db->index[cmd.value]++;
+  if (cmd->value < INDEX_COUNT) {
+    db->index[cmd->value]++;
   }
   else {
     // FIXIT TODO index overflow, just scan table
   }
 }
 
-void db_unset(cmd_t cmd, dorkdb_t *db) {
-  int idx = hash(cmd.var);
+int db_get(cmd_t *cmd, dorkdb_t *db) {
+  int idx = hash(cmd->var);
+  bucket_t *cur = db->buckets[idx];
+  while (cur != NULL) {
+    if (strcmp(cmd->var, cur->name) == 0) return cur->value;
+    cur = cur->next;
+  }
+  return -666;
+}
+
+void db_unset(cmd_t *cmd, dorkdb_t *db) {
+  int idx = hash(cmd->var);
   int removed = 1;
   bucket_t *bucket = db->buckets[idx];
   bucket_t *prev, *cur = bucket;
   while (cur != NULL) {
-    if (strcmp(cur->name, cmd.var) == 0) {
+    if (strcmp(cur->name, cmd->var) == 0) {
       prev->next = cur->next;
       free(cur);
       cur = NULL;
@@ -93,8 +155,8 @@ void db_unset(cmd_t cmd, dorkdb_t *db) {
     cur = cur->next;
   }
 
-  if (cmd.value < INDEX_COUNT && removed) {
-    db->index[cmd.value]--;
+  if (cmd->value < INDEX_COUNT && removed) {
+    db->index[cmd->value]--;
   }
   else {
     // FIXIT TODO index overflow, just scan table
@@ -103,12 +165,15 @@ void db_unset(cmd_t cmd, dorkdb_t *db) {
 
 int hash(char *s) {
   int sum = 0;
-  while (*s) sum += *s;
+  while (*s) {
+    sum += *s;
+    s++;
+  }
   return (sum*351779) % BUCKET_COUNT;
 }
 
 void halt_db(dorkdb_t *db) {
-  if (db->buckets != NULL) {
+  if (db->buckets[0] != NULL) {
     // FIXIT free buckets
   }
   if (db->journal != NULL) {
